3ee597dc632a96815b6b31edee2abd0582806a0e
chonam93 <chonam0916@gmail.com>
2021-04-29 18:26:39 +0900

    programmers level2 solution

---
+++ [level2]주식가격.py
@@ -0,0 +1,16 @@
+def solution(prices):
+    answer = []
+    for i in range(len(prices)):
+        count = 0
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                count += 1        
+            elif prices[i] > prices[j]:
+                count += 1
+                break
+            else:
+                break
+        answer.append(count)
+    return answer
+
+print(solution([1, 2, 3, 2, 3]	))
